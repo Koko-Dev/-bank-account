56b240d7b483dc278d04815d4b7521b2e8307a2b
Koko-Dev <koko.webdev@gmail.com>
2023-11-15 12:47:47 -0500

    App.js:  Add bare bones buttons for each thing we want to have done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+const initialState = {
+  balance: 0, loan: 0, isActive: false,
+};
+
+
 function App() {
   return (
-      <div>
-        App
+      <div className='App'>
+        <h1>useReducer Bank Account</h1>
+        <p>Balance: X</p>
+        <p>Loan: X</p>
+        
+        <p>
+          <button>Open Account</button>
+        </p>
+        <p>
+          <button>Deposit 150</button>
+        </p>
+        <p>
+          <button>Withdraw 50</button>
+        </p>
+        <p>
+          <button>Request a loan of 5000</button>
+        </p>
+        <p>
+          <button>Pay loan</button>
+        </p>
+        <p>
+          <button>Close Account</button>
+        </p>
       </div> );
 }
 
